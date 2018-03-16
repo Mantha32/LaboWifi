aa7e8e3c42bbe6932950220179fe4efbd130674f
Yosra <harbaoui-yosra@hotmail.fr>
2018-03-16 16:03:06 +0100

    Update monitorOn.sh

--- monitorOn.sh
+++ monitorOn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#This script put the interface on and on monitor mode.
+#You can replace 'wlx00c0ca3fb74a' with the name of your interface  
+# Author: Yosra Harbaoui, Iando Rafidimalala
 
-#sudo ip link set $1 name wlan0
 sudo ip link set wlx00c0ca3fb74a down
 sudo iwconfig wlx00c0ca3fb74a  mode monitor
 sudo ip link set wlx00c0ca3fb74a up
